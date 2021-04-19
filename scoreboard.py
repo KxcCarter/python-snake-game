8b921f467e5a8075e9db4fb6a9caed176e33fc28
Kenneth Carter <kxccarter@gmail.com>
2021-04-19 13:14:29 -0500

    refactors scoreboard.py. Adds high_score property

--- scoreboard.py
+++ scoreboard.py
@@ -6,17 +6,27 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 275)
-        self.write(f"Score: {self.score}", False, align=ALIGN, font=FONT)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} | High Score: {self.high_score}", False, align=ALIGN, font=FONT)
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", False, align=ALIGN, font=FONT)
 
     def add_point(self):
-        self.clear()
         self.score += 1
-        self.write(f"Score: {self.score}", False, align=ALIGN, font=FONT)
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", False, align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
